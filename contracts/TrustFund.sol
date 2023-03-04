20caee904f825134290126e9e3ff03269f4a0533
Guy1m0 <zjc.kingdom@gmail.com>
2023-03-04 14:36:32 -0700

    Finish simple exploit contract

--- contracts/TrustFund.sol
+++ contracts/TrustFund.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.7.0 <0.9.0;
+import "hardhat/console.sol";
+
 
 contract TrustFund {
   address owner;
@@ -7,17 +9,20 @@ contract TrustFund {
   mapping (address => uint256) balances;
   Logger public TrustLog;
 
-  constructor (uint256 _minDeposit, address _logger) public payable {
+  constructor (address _logger) public payable {
     owner = msg.sender;
-    minDeposit = _minDeposit;
+    minDeposit = 1e17;
     TrustLog = Logger(_logger);
   }
 
   function deposit() public payable returns (bool) {
-    if (msg.value > minDeposit) {
+    if (msg.value >= minDeposit) {
       balances[msg.sender]+=msg.value;
+      console.log("enough msg.value");
       TrustLog.LogTransfer(msg.sender,msg.value,"deposit");
     } else {
+        //console.log("value:", msg.value);
+      console.log("Not enough");
       TrustLog.LogTransfer(msg.sender,msg.value,"depositFailed");
     }
   }
@@ -25,6 +30,7 @@ contract TrustFund {
   function withdraw(uint256 _amount) public {
     if(_amount <= balances[msg.sender]) {
       (bool s, ) = msg.sender.call{value: _amount}("");
+      console.log("Finish transfer?")
       if(s) {
         balances[msg.sender] -= _amount;
         TrustLog.LogTransfer(msg.sender, _amount, "withdraw");
